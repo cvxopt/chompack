30dc888eb5197b606527b4cdc39ae79a1d693d0b
Martin S. Andersen <martin.skovgaard.andersen@gmail.com>
2026-03-02 18:17:52 +0100

    Update doc conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -92,10 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd: # only import and set the theme if we're building docs locally
-    html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 #else:
 #    # use MathJax on RTD
